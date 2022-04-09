90a1902e4b007d23867b3c9ee1f6954b5c8895a4
luiz-cazarin <57879201+luiz-cazarin@users.noreply.github.com>
2022-04-08 23:56:01 -0300

    Portfolio in react.js

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css'
+
+export default function Navbar () {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <p>LC.</p>
+            </div>
+            <div className={styles.listbtn}>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
+                        <Link to="/">HOME</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/">PROJECTS</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/">CONTACT</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/">LOGIN</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
